149453b424881f24cd01829ee3794c3cf5bd7741
Brice Leroy <bbrriiccee@gmail.com>
2017-03-24 14:14:39 -0700

    process only if words entered

--- packager.py
+++ packager.py
@@ -17,34 +17,31 @@ def main():
         # read all the words the user entered stripped of surrounding spaces
         words = [word.strip() for word in re.split('\W+', cmd) if word.strip()]
 
-        # if you didn't enter anything and press <ENTER>
-        if not words:
-            continue
+        if words:
+            # the first word is the main command
+            command = words[0]
 
-        # the first word is the main command
-        command = words[0]
+            # the following words are the arguments
+            arguments = words[1:]
 
-        # the following words are the arguments
-        arguments = words[1:]
+            if command == 'DEPEND':
+                pkg.depend(*arguments)
 
-        if command == 'DEPEND':
-            pkg.depend(*arguments)
+            elif command == 'INSTALL':
+                pkg.install(arguments[0])
 
-        elif command == 'INSTALL':
-            pkg.install(arguments[0])
+            elif command == 'REMOVE':
+                pkg.remove(arguments[0])
 
-        elif command == 'REMOVE':
-            pkg.remove(arguments[0])
+            elif command == 'LIST':
+                for package_name in pkg.list():
+                    print("\t%s" % package_name)
 
-        elif command == 'LIST':
-            for package_name in pkg.list():
-                print("\t%s" % package_name)
+            elif command == 'END':
+                sys.exit(0)
 
-        elif command == 'END':
-            sys.exit(0)
-
-        else:
-            print "ERROR: unknow command %s" % (command)
+            else:
+                print "ERROR: unknow command %s" % (command)
 
 if __name__ == '__main__':
     main()
